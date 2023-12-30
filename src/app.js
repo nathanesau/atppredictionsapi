c6940c6b58f2221e1f396f9bc7d5313e6cc47800
Nathan Esau <nathanesau1@gmail.com>
2023-12-30 01:44:15 -0800

    get lambda api working with auth

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const cors = require("cors");
+const { auth } = require("express-oauth2-jwt-bearer");
 const app = express();
+const fetch = require("node-fetch");
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+//const appOrigin = `https://nathanesau.github.io/atppredictions/external-api`;
+app.use(cors({ origin: '*' }));
+
+const checkJwt = auth({
+    audience: "atppredictions",
+    issuerBaseURL: "https://dev-e7x276delr7bwmo8.us.auth0.com/",
+    algorithms: ["RS256"],
+});
+
+app.get('/', checkJwt, async (req, res) => {
+
+    const userInfo = await fetch('https://dev-e7x276delr7bwmo8.us.auth0.com/userinfo', {
+        headers: {
+            Authorization: `Bearer ${req.auth.token}`
+        }
+    });
+
+    const body = await userInfo.text();
+
+
+  res.send({ msg: `hello world ${body}` });
 });
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ const app = require('./app');
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) =>  awsServerlessExpress.proxy(server, event, context);
+
+if (process.env.LOCAL) {
+    app.listen(3001, () => console.log(`API Server listening on port ${3001}`));
+}
