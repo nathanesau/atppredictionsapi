a376962cf846937edd6f09fd2cc1c92f11579e2e
Nathan Esau <nathanesau1@gmail.com>
2023-12-30 02:15:59 -0800

    finish api skeleton

--- src/app.js
+++ src/app.js
@@ -4,27 +4,44 @@ const { auth } = require("express-oauth2-jwt-bearer");
 const app = express();
 const fetch = require("node-fetch");
 
-//const appOrigin = `https://nathanesau.github.io/atppredictions/external-api`;
+// TODO: manage this via secrets
+const domain = "dev-e7x276delr7bwmo8.us.auth0.com";
+
 app.use(cors({ origin: '*' }));
 
 const checkJwt = auth({
     audience: "atppredictions",
-    issuerBaseURL: "https://dev-e7x276delr7bwmo8.us.auth0.com/",
+    issuerBaseURL: `https://${domain}/`,
     algorithms: ["RS256"],
 });
 
-app.get('/', checkJwt, async (req, res) => {
-
-    const userInfo = await fetch('https://dev-e7x276delr7bwmo8.us.auth0.com/userinfo', {
+async function getUserId(req) {
+    const userInfo = await fetch(`https://${domain}/userinfo`, {
         headers: {
             Authorization: `Bearer ${req.auth.token}`
         }
     });
 
-    const body = await userInfo.text();
+    const body = await userInfo.json();
 
+    // use email as userId
+    return body.email;
+}
 
-  res.send({ msg: `hello world ${body}` });
+function saveprediction(userId, body) {
+    return `user ${userId} saveprediction`;
+}
+
+app.get('/', checkJwt, async (req, res) => {
+
+    const method = req.query.method;
+    const userId = await getUserId(req);
+
+    if (method === 'saveprediction') {
+        res.send({ msg: saveprediction(userId, req.body) });
+    } else {
+        res.send({ msg: "undefined method passed!"});
+    }
 });
 
 module.exports = app;
